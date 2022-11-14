c30f1b07adbefbc3532edac613eb1f75770b7df7
Eugene Park <e.park5336@gmail.com>
2022-11-13 22:22:55 -0500

    got the questions to cyle through

--- Assets/script.js
+++ Assets/script.js
@@ -7,6 +7,11 @@ var h2El = document.getElementById('questionDisplay');
 var multchoiceListEl = document.getElementById('mult');
 var starBtnEl = document.getElementById('startBtn');
 var quizBoxbannerEl = document.getElementById('quizBoxbanner');
+var li1 = document.getElementById('multichoice1')
+var li2 = document.getElementById('multichoice2')
+var li3 = document.getElementById('multichoice3')
+var li4 = document.getElementById('multichoice4')
+var timerEl = document.getElementById('timebox')
 // end listed html elements
 
 // question pool 
@@ -33,22 +38,38 @@ for (i=0; i < 14; ++i){
     questionPool[i] = eval(`question${(i+1)}`)
 }
 
-function setTime(timerEl,) {
+// // timer function
+
+function updateTime () {
+  
+}
+
+function countDown(secondsLeft) {
     var timerInterval = setInterval(function() {
       secondsLeft--;
       timerEl.textContent = secondsLeft;
   
-      if(secondsLeft === 0) {
-        // Stops execution of action at set interval
+      if(secondsLeft < 0) {
         clearInterval(timerInterval);
-        timerEl.textContent = "times up!"
+        timerEl.textContent = "Out of Time!"
+        li1.setAttribute("class", "finished");
+        li2.setAttribute("class", "finished");
+        li3.setAttribute("class", "finished");
+        li4.setAttribute("class", "finished");
+        h2El.textContent = (`And We're done!`);
+        li1.textContent = (`you got ${points} out of ${questionPool.length} right`);
+        li2.textContent = (`with ${secondsLeft}s left on the clock`);
+        li3.textContent = ('');
+        li4.textContent = ('');
+        results= {points, secondsLeft}
       }
   
     }, 1000);
   }
+  // timer function
 
 // This function fills the multiple choice text content with one correct answer and three randomized wrong answers from the rest of the pool
-  function queryQuestion(n,li1,li2,li3,li4) {
+  function queryQuestion(n) {
     h2El.textContent = `${questionPool[n].question}`;
     // collects all other "incorrect answers to populate into the other choices"
     var otheranswerPool = [];
@@ -57,6 +78,7 @@ function setTime(timerEl,) {
       }
       otheranswerPool.splice(n,1)
     // creates array of three wrong answers pulled from the whole pool and the right answer
+
     var choiceArray =[];
     choiceArray[0] = `${questionPool[n].correctAnswer}`;
       var i = 1
@@ -81,41 +103,90 @@ function setTime(timerEl,) {
     li2.textContent = `${choiceArray[placement[1]]}`;
     li3.textContent = `${choiceArray[placement[2]]}`;
     li4.textContent = `${choiceArray[placement[3]]}`;
-    
-    return console.log(choiceArray, placement);
+
+    // because the correct answer is initially placed in the first element of the choice array, which then
+    // the coordinates are given to it by the first element of the placement array. we can determine where the correct answer is.
+    eval(`li${placement[(0)]+1}`).setAttribute('id', "correctChoice");
+    eval(`li${placement[(1)]+1}`).setAttribute('id', "incorrectChoice");
+    eval(`li${placement[(2)]+1}`).setAttribute('id', "incorrectChoice");
+    eval(`li${placement[(3)]+1}`).setAttribute('id', "incorrectChoice");
   }
 // END This function fills the multiple choice text content with one correct answer and three randomized wrong answers from the rest of the pool
   
 
 // Inital click resets the page for questions and starts the inital timer
 
-var secondsLeft = 5;
-var addtime = 0;
 starBtnEl.addEventListener("click", function() {
-    starBtnEl.textContent = ''
-    starBtnEl.setAttribute('id', 'blank')
-    starBtnEl.setAttribute('class', 'blank')
-    var li1 = document.createElement('li');
-    var li2 = document.createElement('li');
-    var li3 = document.createElement('li');
-    var li4 = document.createElement('li');
-    var timerEl = document.createElement('div')
+    starBtnEl.textContent = '';
+    starBtnEl.setAttribute('id', 'blank');
+    starBtnEl.setAttribute('class', 'blank');
+    timerEl.setAttribute("class", 'timer');
     li1.setAttribute("class", "choice");
     li2.setAttribute("class", "choice");
     li3.setAttribute("class", "choice");
     li4.setAttribute("class", "choice");
-    li1.setAttribute("id", "multChoice2");
-    li2.setAttribute("id", "multChoice2");
-    li3.setAttribute("id", "multChoice3");
-    li4.setAttribute("id", "multChoice4");
-    timerEl.setAttribute("class", 'timer');
-    multchoiceListEl.appendChild(li1);
-    multchoiceListEl.appendChild(li2);
-    multchoiceListEl.appendChild(li3);
-    multchoiceListEl.appendChild(li4);
-    quizBoxbannerEl.appendChild(timerEl);
-    queryQuestion(1,li1,li2,li3,li4)
-    setTime (timerEl)
-}
-)
+    // loads up first question then sets event listener to each li item
+    queryQuestion(1);
+    var choiceEl = document.querySelectorAll('.choice');
+    secondsLeft = 30
+    var points = 0
+    n = 2
+    var results = [];
+    for (i=0; i < choiceEl.length; ++i) {
+    choiceEl[i].addEventListener("click", function(event){
+        if (event.target.id === 'correctChoice' && (n < questionPool.length)) {
+        secondsLeft = secondsLeft + 20;
+        points = points + 1;
+        queryQuestion(n);
+        // setTime();
+      }
+        else if (event.target.id === 'incorrectChoice' && (n < questionPool.length)) {
+        secondsLeft = secondsLeft - 20;
+        points = points + 0;
+        queryQuestion(n);
+        // setTime();
+      } 
+        else {
+        li1.setAttribute("class", "finished");
+        li2.setAttribute("class", "finished");
+        li3.setAttribute("class", "finished");
+        li4.setAttribute("class", "finished");
+        h2El.textContent = (`And We're done!`);
+        li1.textContent = (`you got ${points} out of ${questionPool.length} right`);
+        li2.textContent = (`with ${secondsLeft}s left on the clock`);
+        li3.textContent = ('');
+        li4.textContent = ('');
+        results= {points, secondsLeft}
+      }
+    n = n + 1;
+  })}
+    })
 
+
+
+
+
+
+
+
+
+
+// var choiceEl = document.querySelectorAll('.choice');
+// var points = 0
+
+// for (i=0; i < choiceEl.length; ++i) {
+//   choiceEl[i].addEventListener("click", function(event){
+//         if (event.target.id === 'correctChoice') {
+//         var secondsLeft = secondsLeft + 20;
+//         points = points + 1;
+//         queryQuestion(i+1,li1,li2,li3,li4);
+//         setTime();
+//     }
+//     else if (event.target.id === 'incorrectChoice') {
+//         var secondsLeft = secondsLeft - 20;
+//         points = points + 0;
+//         queryQuestion(i+1,li1,li2,li3,li4);
+//         setTime();
+//     }
+//   })
+// }
